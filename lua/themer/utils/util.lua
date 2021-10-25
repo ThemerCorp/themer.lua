b4a6b508fda9cb43cb51d7a6dcd31db994fc4d8e
NarutoXY <79555780+NarutoXY@users.noreply.github.com>
2021-10-25 18:26:14 +0530

    fix the autocmd

--- lua/themer/utils/util.lua
+++ lua/themer/utils/util.lua
@@ -114,7 +114,7 @@ function util.load(theme)
 		util.terminal(theme.colors)
 	end
 	
-	vim.cmd [[au ColorSchemePre * :lua require"custom.noice_dark".check_change()]]
+	vim.cmd [[au ColorSchemePre * :lua require("themer.utils.util").check_change()]]
 	vim.api.nvim__set_hl_ns(ns)
 end
 
