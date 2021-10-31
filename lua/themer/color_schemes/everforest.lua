148b9d27bd0e6017a851a1c8246e0504404280cc
max397574 <81827001+max397574@users.noreply.github.com>
2021-10-31 12:09:06 +0100

    add everforest theme (#12)
    * feat(themes): added onedark
    
    * feat(themes): added everforest

---
+++ lua/themer/color_schemes/everforest.lua
@@ -0,0 +1,21 @@
+local colors = {
+    flavour = "light",
+    bg = "#2b3339",
+    bg_alt = "#323c41",
+    bg_float = "#3a4248",
+    inactive = "#868d80",
+    subtle = "#868d80",
+    fg = "#d3c6aa",
+    red = "#e67e80",
+    yellow = "#dbbc7f",
+    orange = "#e69875",
+    blue = "#7fbbb3",
+    green = "#a7c080",
+    magenta = "#d699b6",
+    highlight = "#3a4248",
+    highlight_inactive = "#323c41",
+    highlight_overlay = "#323c41",
+    none = "NONE",
+}
+
+return colors
