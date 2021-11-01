5224a38a8558f5061cb71adc15848b348689d40d
shift <shiftkeydeveloper@gmail.com>
2021-11-01 17:14:59 +0300

    Create `random` theme (#19)

---
+++ colors/random.lua
@@ -0,0 +1,42 @@
+-- Get path to json.lua
+local relative = require("lua.themer.utils.json").get_cfd()
+-- Remove lua/themer/utils/json.lua substring for both OS
+relative = relative:gsub("lua\\themer\\utils\\json.lua", "")
+relative = relative:gsub("lua/themer/utils/json.lua", "")
+
+-- add wildcard to themes to path
+relative = table.concat({relative, "colors/*.lua"})
+
+-- expand wildcards; get all themes' pathes
+local themes = vim.fn.expand(relative)
+
+-- remove wildcard from our path to theme folder
+relative = relative:gsub("*.lua", "")
+
+-- leave only themename.lua\n
+themes = themes:gsub(relative, "")
+
+-- leave only themename\n
+themes = themes:gsub(".lua", "")
+
+-- remove random from themes
+themes = themes:gsub("random\n", "")
+
+-- split strings and create table
+local function split_table(s, delimiter)
+    local result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+-- create themes table
+themes = split_table(themes, "\n")
+
+-- random stuff
+math.randomseed(os.clock())
+local index = math.floor(math.random() * #themes) + 1
+
+-- load theme
+require("themer").load(themes[index])
