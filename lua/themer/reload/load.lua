20b9bf0dd1e4d0cc101c38e19da7af483165ec1e
narutoxy <79555780+NarutoXY@users.noreply.github.com>
2021-12-24 20:41:18 +0530

    fix rose pine | alpha vers of live reload

---
+++ lua/themer/reload/load.lua
@@ -0,0 +1,21 @@
+local currentFileTail = vim.fn.expand("%:t"):gsub(".lua", "")
+local currentFilePath = vim.fn.expand("%"):gsub(".lua", "")
+
+local load = function()
+	local ok, colorscheme = pcall(require, currentFilePath)
+	if ok then
+		print("ok")
+		-- bear with me for too many pcalls
+		pcall(
+			require("themer.utils"),
+			pcall(
+				require("themer.core.mapper"),
+				colorscheme,
+				currentFileTail
+			)
+		)
+		print("done")
+	end
+end
+
+return load
