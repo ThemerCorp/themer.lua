ffcbb1db61436627e201e0f7b223cf24fb44e2c0
shift-d <shiftkeydeveloper@gmail.com>
2021-12-30 16:19:08 +0300

    update kurai

--- lua/themer/color_schemes/kurai.lua
+++ lua/themer/color_schemes/kurai.lua
@@ -1,24 +1,44 @@
 local colors = {
     flavour = "dark",
     bg = "#1a1a1a",
-    bg_alt = "#333333",
-    bg_float = "#0f0f0f",
+    bg_alt = "#1f1f1f",
+    bg_float = "#141414",
     inactive = "#393b44",
     subtle = "#526175",
     fg = "#d2c1e6",
-    red = "#ff2929",
-    green = "#7dff4a",
-    blue = "#5c6cff",
-    yellow = "#ffd026",
-    orange = "#ee6d85",
+    red = "#f76e6e",
+    green = "#82f76e",
+    blue = "#6e72f7",
+    yellow = "#ffdd33",
+    orange = "#ff8833",
     white = "#f1e6ff",
     magenta = "#9e39e6",
     funcbuiltin = "#a485dd",
     highlight = "#212121",
     highlight_overlay = "#212111",
-    highlight_inactive = "#212121",
+    highlight_inactive = "#222222",
     delimiter = "#58cd8b",
     none = "NONE",
+}
+
+colors.remaps = {
+    plugins = {
+        telescope = {
+            TelescopeSelection = { bg = colors.highlight_inactive },
+            TelescopeMatching = { fg = colors.blue },
+            TelescopeBorder = { fg = colors.bg_alt, bg = colors.bg_alt },
+            TelescopePromptCounter = { fg = colors.funcbuiltin },
+            TelescopePromptPrefix = { fg = colors.blue, bg = colors.bg_alt },
+            TelescopeResultsTitle = { fg = colors.bg_alt, bg = colors.blue },
+            TelescopePreviewTitle = { fg = colors.bg_alt, bg = colors.green },
+            TelescopePromptTitle = { fg = colors.bg_alt, bg = colors.yellow },
+            TelescopePreviewNormal = { bg = colors.bg_alt },
+            TelescopeResultsNormal = { bg = colors.bg_alt },
+            TelescopePromptNormal = { fg = colors.fg, bg = colors.bg_alt },
+            TelescopePromptBorder = { fg = colors.highlight_inactive, bg = colors.bg_alt },
+            TelescopeSelectionCaret = { fg = colors.bg_alt, bg = colors.bg_alt },
+        }
+    }
 }
 
 return colors
