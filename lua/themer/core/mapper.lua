89d6501c1c23ff3c71915cc1cee03120e408b654
narutoxy <79555780+NarutoXY@users.noreply.github.com>
2022-01-07 22:10:55 +0530

    mend

--- lua/themer/core/mapper.lua
+++ lua/themer/core/mapper.lua
@@ -243,24 +243,24 @@ local function get_base(cp)
         --     BufferVisibleSign = { link = "ThemerSubtleFloat"  },
         -- },
 
-        bufferline = {
-            BufferLineFill = { bg = cp.bg_float },
-            BufferLineBackground = { fg = cp.subtle, bg = cp.inactive },
-            BufferLineBufferVisible = { fg = cp.subtle, bg = cp.inactive },
-            BufferLineBufferSelected = { fg = cp.fg, bg = cp.bg },
-            BufferLineTab = { fg = cp.subtle, bg = cp.bg },
-            BufferLineTabSelected = { fg = cp.red, bg = cp.blue },
-            BufferLineTabClose = { fg = cp.red, bg = cp.inactive },
-            BufferLineIndicatorSelected = { fg = cp.bg, bg = cp.bg },
-            -- separators
-            BufferLineSeparator = { fg = cp.inactive, bg = cp.inactive },
-            BufferLineSeparatorVisible = { fg = cp.inactive, bg = cp.inactive },
-            BufferLineSeparatorSelected = { fg = cp.inactive, bg = cp.inactive },
-            -- close buttons
-            BufferLineCloseButton = { fg = cp.subtle, bg = cp.inactive },
-            BufferLineCloseButtonVisible = { fg = cp.subtle, bg = cp.inactive },
-            BufferLineCloseButtonSelected = { fg = cp.red, bg = cp.bg },
-        },
+        -- bufferline = {
+        --     BufferLineFill = { bg = cp.bg_float },
+        --     BufferLineBackground = { fg = cp.subtle, bg = cp.inactive },
+        --     BufferLineBufferVisible = { fg = cp.subtle, bg = cp.inactive },
+        --     BufferLineBufferSelected = { fg = cp.fg, bg = cp.bg },
+        --     BufferLineTab = { fg = cp.subtle, bg = cp.bg },
+        --     BufferLineTabSelected = { fg = cp.red, bg = cp.blue },
+        --     BufferLineTabClose = { fg = cp.red, bg = cp.inactive },
+        --     BufferLineIndicatorSelected = { fg = cp.bg, bg = cp.bg },
+        --     -- separators
+        --     BufferLineSeparator = { fg = cp.inactive, bg = cp.inactive },
+        --     BufferLineSeparatorVisible = { fg = cp.inactive, bg = cp.inactive },
+        --     BufferLineSeparatorSelected = { fg = cp.inactive, bg = cp.inactive },
+        --     -- close buttons
+        --     BufferLineCloseButton = { fg = cp.subtle, bg = cp.inactive },
+        --     BufferLineCloseButtonVisible = { fg = cp.subtle, bg = cp.inactive },
+        --     BufferLineCloseButtonSelected = { fg = cp.red, bg = cp.bg },
+        -- },
 
         cmp = {
             CmpItemKind = { fg = cp.magenta },
