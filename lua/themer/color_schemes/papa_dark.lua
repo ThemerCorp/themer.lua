38ae03bf595615b6c32e3ccc99dc144cc181430f
MordechaiHadad <mordechai.hadad01@gmail.com>
2022-01-11 19:26:31 +0200

    changed(papadark): wip new color_palette

--- lua/themer/color_schemes/papa_dark.lua
+++ lua/themer/color_schemes/papa_dark.lua
@@ -1,5 +1,4 @@
 local colors = {
-    flavour = "dark",
     bg = "#2e3440",
     fg = "#E5E9F0",
     bg_alt = "#2b2e36",
@@ -19,26 +18,90 @@ local colors = {
     light_blue = "#8fc6e3",
     cyan = "#4EC9B0",
     vertsplit = "#373e4a",
-    integer = "#B4CDA8",
+    number = "#B4CDA8",
     string = "#A3BE8C",
     line_number = "#4C566A",
     line_color = "#3B4252",
     visual_grey = "#3E4452",
     interface_color = "#84D9AA",
 }
-colors.groups = {
-    styles = {
-        keyword = { fg = colors.blue },
-        comment = { fg = colors.comment },
-        punc = { fg = colors.blue },
-        variable = { fg = colors.light_blue },
-        parameter = { fg = colors.light_blue },
-        func = { fg = colors.yellow },
-        string = { fg = colors.string },
+
+local color_palette = {
+    red = colors.red,
+    yellow = colors.yellow,
+    orange = colors.orange,
+    magenta = colors.magenta,
+    blue = colors.blue,
+    green = colors.green,
+    cyan = colors.cyan,
+
+    -- The new shiny boy
+    fg = colors.fg,
+    diff = {
+        add = colors.green,
+        remove = colors.red,
+        text = colors.blue,
+        change = colors.yellow,
     },
-    diagnostics = { colors = { hint = colors.blue } },
+    accent = colors.blue,
+    search_result = { fg = colors.magenta, bg = colors.bg, telescope = colors.blue },
+    match = colors.fg,
+    dimmed = {
+        inactive = colors.inactive,
+        subtle = colors.subtle,
+    },
+    bg = {
+        base = colors.bg,
+        alt = colors.bg_alt,
+        selected = colors.bg_float,
+    },
+    border = colors.blue,
+    syntax = {
+        ["function"] = colors.blue,
+        variable = colors.red,
+        include = colors.red,
+        keyword = colors.blue,
+        struct = colors.red,
+        string = colors.string,
+        field = colors.light_blue,
+        parameter = colors.light_blue,
+        property = colors.yellow,
+        punctuation = colors.blue,
+        constructor = colors.interface_color,
+        operator = colors.blue,
+        preproc = colors.red,
+        constant = colors.blue,
+        todo = { fg = colors.bg, bg = colors.yellow },
+        number = colors.number,
+        comment = colors.comment,
+        type = colors.red,
+    },
+    built_in = {
+        ["function"] = colors.yellow,
+        type = colors.yellow,
+        variable = colors.light_blue,
+        keyword = colors.blue,
+        constant = colors.blue,
+    },
+    diagnostic = {
+        error = colors.red,
+        warn = colors.yellow,
+        info = colors.red,
+        hint = colors.red,
+    },
+    uri = colors.red,
+    pum = {
+        fg = colors.line_color,
+        bg = colors.bg,
+        sbar = colors.visual_grey,
+        thumb = colors.fg,
+    },
+    heading = {
+        h1 = colors.red,
+        h2 = colors.red,
+    },
 }
-colors.remaps = {
+color_palette.remaps = {
     base = {
         Normal = { fg = colors.fg, bg = colors.bg },
         ColorColumn = { fg = colors.red },
@@ -65,7 +128,7 @@ colors.remaps = {
         FloatBorder = { fg = colors.fg },
         Constant = { fg = colors.blue },
         Character = { link = "String" },
-        Number = { fg = colors.integer },
+        Number = { fg = colors.number },
         Boolean = { link = "Constant" },
         Float = { link = "Number" },
 
@@ -140,4 +203,4 @@ colors.remaps = {
     langs = { md = { markdownCode = { fg = colors.string } } },
 }
 
-return colors
+return color_palette
