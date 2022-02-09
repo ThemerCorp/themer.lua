e0242be1b48aab56c5dcc7b9f130debd523596d3
Astro (Naruto Uzumaki) <github@sandy007.anonaddy.com>
2022-02-09 18:36:51 +0530

    fix hig remaps for particular colorscheme

--- lua/themer/modules/core/init.lua
+++ lua/themer/modules/core/init.lua
@@ -3,7 +3,7 @@
 local load = function(cs)
   local color_palette = require("themer.modules.core.api").get_cp(cs)
   if color_palette then
-    require("themer.modules.core.utils").load_mapper_higs(require("themer.modules.core.mapper")(color_palette), cs)
+    require("themer.modules.core.utils").load_mapper_higs(require("themer.modules.core.mapper")(color_palette, cs), cs)
   end
 end
 
