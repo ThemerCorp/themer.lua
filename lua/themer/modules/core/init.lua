51807512a98e6b5101beb43e8498e6affaae4b1e
Astro <github@sandy007.anonaddy.com>
2022-04-10 18:07:23 +0530

    allow custom cs

--- lua/themer/modules/core/init.lua
+++ lua/themer/modules/core/init.lua
@@ -1,7 +1,12 @@
 --- loads the colorscheme
 --- @param cs string colorscheme name
 local load = function(cs)
-  local color_palette = require("themer.modules.core.api").get_cp(cs)
+  local color_palette
+  if type(cs) == "table" then
+    color_palette = cs
+  else
+    color_palette = require("themer.modules.core.api").get_cp(cs)
+  end
   if color_palette then
     require("themer.modules.core.utils").load_mapper_higs(require("themer.modules.core.mapper")(color_palette, cs), cs)
   end
